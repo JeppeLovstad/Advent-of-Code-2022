3d6742af2915b2261b8de9ac75be9edee0424c6e
jeppe <jeppemlv@gmail.com>
2022-12-01 17:40:04 +0100

    day 1 done

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -1,50 +1,44 @@
-use std::cmp::Reverse;
-
 use input_reader;
+use std::cmp::Reverse;
 
 #[derive(Debug, Clone)]
 struct Elf {
     total_calories: i32,
-    calories: Vec<i32>
 }
 
 fn main() {
     let input = prepare_input();
-
-    let elves: Vec<Elf> = Vec::new();
-    for line in input {
-
-        if line == "" {
-
-
-            continue;
-        }
-
-        let cal:i32 = line.parse::<i32>().unwrap();
-        elf.total_calories += cal;
-        elf.calories.push(cal);
-    }
-    println!("{:?}", elf);
-    elves.push(elf);
+    let grouped_input = group_input(input);
+    let elves: Vec<Elf> = get_elves(grouped_input);
 
     let max = elves.iter().map(|x| x.total_calories).max();
 
     println!("{:?}", max);
 
-    elves.sort_by_key(|x| Reverse(x.total_calories));
-    let sum: i32 = elves[0..=2].iter().map(|x| x.total_calories).sum();
+    let mut sorted_elves = elves.clone();
+    sorted_elves.sort_by_key(|x| Reverse(x.total_calories));
+    let sum: i32 = sorted_elves[0..=2].iter().map(|x| x.total_calories).sum();
     println!("{:?}", sum);
 }
 
-fn group_input(input:Vec<String>) -> Vec<Vec<I32>>{
-
+fn group_input(input: Vec<String>) -> Vec<Vec<i32>> {
+    let grouped_input = input.split(|x| x == "");
+    let parsed_grouped_input =
+        grouped_input.map(|x| x.into_iter().map(|y| parse_string_to_int(y)).collect());
+    parsed_grouped_input.collect()
 }
 
-fn get_elf(calories:&Vec<i32>) -> Elf {
-    
-    Elf{
+fn parse_string_to_int(input_string: &String) -> i32 {
+    input_string.parse::<i32>().unwrap()
+}
+
+fn get_elves(grouped_elves: Vec<Vec<i32>>) -> Vec<Elf> {
+    grouped_elves.into_iter().map(|x| get_elf(x)).collect()
+}
+
+fn get_elf(calories: Vec<i32>) -> Elf {
+    Elf {
         total_calories: calories.iter().sum(),
-        calories: calories.to_vec(),
     }
 }
 
