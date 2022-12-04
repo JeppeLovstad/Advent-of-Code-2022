e45220b3e6dfda3d0f63d5237b38a444324bdabc
jeppe <jeppemlv@gmail.com>
2022-12-04 12:28:30 +0100

    day 4

--- day_4/src/main.rs
+++ day_4/src/main.rs
@@ -2,9 +2,85 @@ use input_reader;
 fn main() {
     let input = prepare_input();
 
-    for line in input {
-        println!("{line}")
+    let parse_input_part1: i32 = input.iter().map(|input| parse_input_part1(input)).sum();
+    println!("{:?}", parse_input_part1);
+
+    let parse_input_part2: i32 = input.iter().map(|input| parse_input_part2(input)).sum();
+    println!("{:?}", parse_input_part2)
+}
+
+fn parse_input_part2(input: &String) -> i32 {
+    let (p1, p2) = input.split_once(",").unwrap();
+    let res = if pair_is_overlapping(p1.to_string(), p2.to_string()) {
+        1
+    } else {
+        0
+    };
+    println!("{:?}, {:?}", input, res);
+    res
+}
+
+fn pair_is_overlapping(p1: String, p2: String) -> bool {
+    let (p1_start, p1_end) = p1.split_once("-").unwrap();
+    let (p2_start, p2_end) = p2.split_once("-").unwrap();
+
+    let p1_start = parse_string_to_int(p1_start);
+    let p1_end = parse_string_to_int(p1_end);
+    let p2_start = parse_string_to_int(p2_start);
+    let p2_end = parse_string_to_int(p2_end);
+
+    if p1_start <= p2_start && p1_end >= p2_end {
+        return true;
     }
+
+    if p2_start <= p1_start && p2_end >= p1_end {
+        return true;
+    }
+
+    if (p1_start >= p2_start && p1_start <= p2_end) || (p1_end <= p2_start && p1_end >= p2_end)
+    {
+        return true;
+    }
+
+    if (p2_start >= p1_start && p2_start <= p1_end) || (p2_end <= p1_start && p2_end >= p1_end)
+    {
+        return true;
+    }
+
+    false
+}
+
+fn parse_input_part1(input: &String) -> i32 {
+    let (p1, p2) = input.split_once(",").unwrap();
+    if pair_is_completely_overlapping(p1.to_string(), p2.to_string()) {
+        1
+    } else {
+        0
+    }
+}
+
+fn pair_is_completely_overlapping(p1: String, p2: String) -> bool {
+    let (p1_start, p1_end) = p1.split_once("-").unwrap();
+    let (p2_start, p2_end) = p2.split_once("-").unwrap();
+
+    let p1_start = parse_string_to_int(p1_start);
+    let p1_end = parse_string_to_int(p1_end);
+    let p2_start = parse_string_to_int(p2_start);
+    let p2_end = parse_string_to_int(p2_end);
+
+    if p1_start <= p2_start && p1_end >= p2_end {
+        return true;
+    }
+
+    if p2_start <= p1_start && p2_end >= p1_end {
+        return true;
+    }
+
+    false
+}
+
+fn parse_string_to_int(input_string: &str) -> i32 {
+    input_string.parse::<i32>().unwrap()
 }
 
 fn prepare_input() -> Vec<String> {
